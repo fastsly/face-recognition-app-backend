353b7ef8f36f930b0573a9290feeceb0f82d5752
fastsly <szalai.laszlo.istvan.dev@gmail.com>
2020-10-21 22:15:13 +0300

    further dev

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs')
 
 const app = express()
 app.use(express.json())
@@ -10,9 +11,15 @@ const database ={
             id: '123',
             name: 'John Doe',
             email: 'johndoe@gmail.com',
-            password: 'pass',
             entries: 0,
             joined: new Date()
+        }
+    ],
+    login:[
+        {
+            id:'1234',
+            hash:'',
+            email:'johndoe@gmail.com'
         }
     ]
 }
@@ -22,6 +29,9 @@ app.get('/',(req,res) =>{
 })
 
 app.post('/signin', (req,res) =>{
+    bcrypt.compare("bacon", hash, function(err, res) {
+        // res == true
+    });
     if (req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password){
             res.json('success')
@@ -32,6 +42,16 @@ app.post('/signin', (req,res) =>{
 
 app.post('/register', (req,res) => {
     const { email, name, password } = req.body
+    
+    bcrypt.hash(password, null, null, (err, hash) => {
+        // Store hash in your password DB.
+        console.log(hash)
+        database.login.push({
+            id:'',
+            hash:hash
+        })
+    });
+
     database.users.push({
         id: '126',
         name: name,
@@ -42,6 +62,46 @@ app.post('/register', (req,res) => {
     })
     res.status(200).json(`New user ${database.users[(database.users.length-1)].name} succesfully created`)
 })
+
+app.get('/profile/:id',(req,res) => {
+    const {id} = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id){
+            found = true;
+           return res.json(user)
+        }
+    })
+    if (!found){
+        res.status(404).json('No such user!')
+    }
+})
+
+app.post("/image",(req,res) =>{
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id){
+            user.entries++
+            found = true;
+           return res.json(user)
+        }
+    })
+    if (!found){
+        res.status(404).json('No such user!')
+    }
+
+})
+
+bcrypt.hash("bacon", null, null, function(err, hash) {
+    // Store hash in your password DB.
+});
+
+// // Load hash from your password DB.
+
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
 
 app.listen(3000, () => {
     console.log("Server is running!")
