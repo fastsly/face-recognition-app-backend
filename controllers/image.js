2863e5804470f8b178b0ba40fa2f86c7be07342b
fastsly <szalai.laszlo.istvan.dev@gmail.com>
2020-10-28 19:40:09 +0200

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -19,7 +19,7 @@ const handleApiCall = (req, res) => {
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
       res.json(data);
-    })
+    }).catch(err => res.status(400).json('unable to work with API'))
 }
 const handleImage = (req, res, db) =>{
     const { id } = req.body;
