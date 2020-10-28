0ddc17e1d326c57842e6b42634025454eea85f56
fastsly <szalai.laszlo.istvan.psiho@gmail.com>
2020-10-28 10:46:31 +0200

    updating database

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const profile = require('./controllers/profile')
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "bungeelaci",
-    database: "face-recognition",
+    host: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
